53fe64438ff8ab6fcd343edbca70220d1217b51c
Emil Kopytek <107545485+CeDwukropek@users.noreply.github.com>
2024-01-25 14:28:25 +0100

    working hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import { useTasks } from './useTasks';
+import { Task } from './Task';
+import {v4 as uuid} from 'uuid'
 
 function App() {
-  return(
-    <div>
-      
+  const {tasks, addTask, removeTask, completeTask} = useTasks();
+
+  let ID = uuid()
+
+  return (
+    <div className="App">
+      {tasks.map((item) => (
+        <Task
+          id={item.id}
+          value={item.value}
+          completed={item.completed}
+          delete={() => removeTask(item)}
+          complete={() => completeTask(item)}
+        />
+      ))}
+      <button onClick={() => addTask({id: ID, value: "Test" + ID, completed: false})}>Add</button>
     </div>
-  )
+  );
 }
 
 export default App;
